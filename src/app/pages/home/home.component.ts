196c30ff9e216c69215a7e62edabca701fca4686
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-11 18:23:26 -0500

    final

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component ,OnInit ,Output} from '@angular/core';
+import {FormGroup, FormBuilder,} from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+datos:any[]=[{nombre:'Carlos',apellido:'Portillo',carrera:'Front-end Developer'}]
+
+@Output() repuesta:string='';
+public formLogin!: FormGroup;
+
+constructor(private formBuilder:FormBuilder){
+}
+ngOnInit(): void {
+}
+loadAPI():any{
+  const response = {
+    nombre:'',
+    DNI:'',
+    estado:''
+  };
+
+  this.formLogin.patchValue(response);
+   
+}
+send(): any{
+  console.log(this.formLogin.value)
+}
+
+registrar(nombre:string,apellido:string,carrera:string){
+  this.datos.push({nombre,apellido,carrera})
+  
+ }
+ eliminar(index:number){
+  this.datos.splice(index,1) 
+ }
+}
